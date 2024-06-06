41a4dd68283dd0edbea08f3723a83a8d30c7950d
mert4k <mmd-1903@hotmail.com>
2024-06-06 17:47:31 +0300

    organize imports, xsd settings

--- src/main/java/mmd/assignment/dpa_backend/DpaBackendApplication.java
+++ src/main/java/mmd/assignment/dpa_backend/DpaBackendApplication.java
@@ -3,8 +3,6 @@ package mmd.assignment.dpa_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
